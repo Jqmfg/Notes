441fad21e9f61b93984ceca8b3d9ee4282ebcebe
Jqmfg <therealdavidthien@gmail.com>
2015-07-30 18:10:36 -0700

    Started work on conversion script

---
+++ IntroToLinux/makepdf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Takes all Intro To Linux files and makes a pdf out of them
+cat Chapter1.md >> IntroToLinux.md
+cat Chapter2.md >> IntroToLinux.md
+cat Chapter3.md >> IntroToLinux.md
+cat Chapter4.md >> IntroToLinux.md
+cat Chapter5.md >> IntroToLinux.md
+cat Chapter6.md >> IntroToLinux.md
+cat Chapter7.md >> IntroToLinux.md
+cat Chapter8.md >> IntroToLinux.md
+cat Chapter9.md >> IntroToLinux.md
+cat Chapter10.md >> IntroToLinux.md
+cat Chapter11.md >> IntroToLinux.md
+cat Chapter12.md >> IntroToLinux.md
+cat Chapter13.md >> IntroToLinux.md
+cat Chapter14.md >> IntroToLinux.md
+cat Chapter15.md >> IntroToLinux.md
+cat Chapter16.md >> IntroToLinux.md
+cat Chapter17.md >> IntroToLinux.md
+cat Chapter18.md >> IntroToLinux.md
